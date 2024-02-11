cae6043f6b6338b1d816bfc11cecf2205c69fb03
ransjnr <faradayfoundationgh@gmail.com>
2024-02-11 12:18:02 -0800

    product alerts component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -13,12 +13,13 @@ import { ProductListComponent } from './product-list/product-list.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-    ])
+    ]),
+    ProductAlertsComponent,
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
   ],
   bootstrap: [
     AppComponent
--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './product-alerts.component.css'
 })
 export class ProductAlertsComponent {
-
+  @Input() product: Product | undefined
 }
+
